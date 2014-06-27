5c448a0a2fee065a83dd19750cf6037162bf925f
Douglas Linsmeyer <dlinsmey@nerdery.com>
2014-06-27 15:24:49 -0500

    Added recipes for htop and apache2.

---
+++ ris-application-server/recipes/apache2.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: ris-application-server
+# Recipe:: apache2
+#
+# Copyright 2014, Rant Media Network
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'apache2' do
+    action: remove
+end
---
+++ ris-application-server/recipes/htop.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: ris-application-server
+# Recipe:: htop
+#
+# Copyright 2014, Rant Media Network
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'htop' do
+    action :install
+end
