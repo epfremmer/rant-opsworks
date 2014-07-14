96de4825f46c924fccb3995470fab24ea2b37365
Douglas Linsmeyer <dlinsmey@nerdery.com>
2014-07-14 15:29:31 -0500

    Added new cookbook for trivia stack.

---
+++ rs_trivia/metadata.rb
@@ -0,0 +1,5 @@
+name            "rs_triva"
+description     "Cookbook for the Rant Sports Trivia servers"
+maintainer      "Rant Media Network"
+license         'All rights reserved'
+version         "1.0.0"
---
+++ rs_trivia/recipes/application_install.rb
@@ -0,0 +1,15 @@
+app_web_root = "#{node['rant']['nginx']['web_root']}/#{node['rant']['nginx']['vhost']}"
+
+application node['rant']['application']['name'] do
+    path "#{app_web_root}"
+    owner node['rant']['deploy']['user']
+    group node['rant']['deploy']['group']
+
+    repository node['rant']['deploy']['repository']
+    revision node['rant']['deploy']['branch']
+end
+
+directory "#{app_web_root}/current" do
+    owner node['rant']['deploy']['user']
+    group node['rant']['deploy']['group']
+end
