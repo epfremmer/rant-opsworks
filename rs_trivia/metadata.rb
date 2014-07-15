daaaf9a3de7446a62d36e41893b323984c8f2c1d
Douglas Linsmeyer <dlinsmey@nerdery.com>
2014-07-15 15:25:55 -0500

    Fixed typo.

--- rs_trivia/metadata.rb
+++ rs_trivia/metadata.rb
@@ -1,4 +1,4 @@
-name            "rs_triva"
+name            "rs_trivia"
 description     "Cookbook for the Rant Sports Trivia servers"
 maintainer      "Rant Media Network"
 license         'All rights reserved'
