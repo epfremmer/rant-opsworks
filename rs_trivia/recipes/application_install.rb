8a8a6aba131add51d77b6ca80865e7735b8b6a7f
Dan Eichholz <deichholz@nerdery.com>
2014-08-15 11:15:39 -0500

    Quiz: increase php5 memory for craft due to large category set

--- rs_trivia/recipes/application_install.rb
+++ rs_trivia/recipes/application_install.rb
@@ -36,3 +36,13 @@ link "/etc/php5/fpm/conf.d/20-mcrypt.ini" do
     to "/etc/php5/mods-available/mcrypt.ini"
     only_if "test -L /etc/php5/mods-available/mcrypt.ini"
 end
+
+template "/etc/php5/mods-available/craft.ini" do
+    source "php5_craft_ini.erb"
+    mode '0700'
+end
+
+link "/etc/php5/fpm/conf.d/90-craft.ini" do
+    to "/etc/php5/mods-available/craft.ini"
+    only_if "test -L /etc/php5/mods-available/craft.ini"
+end
