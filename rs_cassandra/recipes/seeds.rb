f7ed2cbca3dcc3841bb475db16fb9a9f230ac68a
epfremme <epfremme@nerdery.com>
2014-08-18 16:04:42 -0500

    - Advance cassandra chef configuration

--- rs_cassandra/recipes/seeds.rb
+++ rs_cassandra/recipes/seeds.rb
@@ -44,7 +44,7 @@ log `printenv AWS_SECRET_ACCESS_KEY`
 env AWS_ACCESS_KEY_ID = aws_access_key_id
 env AWS_SECRET_ACCESS_KEY = aws_secret_access_key
 
-instances = `sudo /usr/local/bin/aws ec2 --region us-east-1 describe-instances --output json`
+instances = `/usr/local/bin/aws ec2 --region us-east-1 describe-instances --output json`
 
 #directory "/root/.aws" do
 #  owner "root"
