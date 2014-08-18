30ff08b98573ae780e5e6998863e219350e4a6c0
epfremme <epfremme@nerdery.com>
2014-08-17 23:38:59 -0500

    - Cassandra chef testing

--- rs_cassandra/metadata.rb
+++ rs_cassandra/metadata.rb
@@ -3,3 +3,5 @@ description     "Cookbook for the Rant Sports Cassandra servers"
 maintainer      "Rant Media Network"
 license         'All rights reserved'
 version         "1.0.0"
+
+depends 'python'
