51c07150aada1a77758d08236128d8cf01285f95
epfremme <epfremme@nerdery.com>
2014-08-18 01:22:49 -0500

    - Cassandra chef testing

--- rs_cassandra/recipes/seeds.rb
+++ rs_cassandra/recipes/seeds.rb
@@ -35,8 +35,8 @@ include_recipe "python"
 
 python_pip "awscli"
 
-export AWS_ACCESS_KEY_ID     = aws_access_key_id
-export AWS_SECRET_ACCESS_KEY = aws_secret_access_key
+ENV['AWS_ACCESS_KEY_ID']     = aws_access_key_id
+ENV['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
 
 instances = `aws ec2 describe-instances --output json`
 
