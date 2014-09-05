2610d3501c03960be0dd2f6085e67a460ac4f79f
epfremme <epfremme@nerdery.com>
2014-09-05 16:31:50 -0500

    - Updated stats cron jobs

--- rs_http/recipes/stats_cron.rb
+++ rs_http/recipes/stats_cron.rb
@@ -14,153 +14,102 @@ app_web_root = "#{node['rant']['nginx']['web_root']}/#{node['rant']['nginx']['vh
 log_file     = "#{node['rant']['nginx']['log_dir']}/#{node['rant']['nginx']['vhost']}.cron.log"
 app_env      = "#{node['rant']['application']['environment_name']}"
 
-# Refresh current games every 5 minutes during active hours
-# */5 0-3,8-23 * * * php app/console rant:stats:cbb:refresh:games:current >/dev/null 2>&1
-# */5 0-3,8-23 * * * php app/console rant:stats:cfb:refresh:games:current >/dev/null 2>&1
-# */5 0-3,8-23 * * * php app/console rant:stats:mlb:refresh:games:current >/dev/null 2>&1
-# */5 0-3,8-23 * * * php app/console rant:stats:nba:refresh:games:current >/dev/null 2>&1
-# */5 0-3,8-23 * * * php app/console rant:stats:nfl:refresh:games:current >/dev/null 2>&1
-# */5 0-3,8-23 * * * php app/console rant:stats:nhl:refresh:games:current >/dev/null 2>&1
+# Refresh current data continuously starting at 8 am, the script will shut down automatically after 3am
+0 8 * * * php app/console rant:stats:cbb:refresh:current >/dev/null 2>&1
+0 8 * * * php app/console rant:stats:cfb:refresh:current >/dev/null 2>&1
+0 8 * * * php app/console rant:stats:mlb:refresh:current >/dev/null 2>&1
+0 8 * * * php app/console rant:stats:nba:refresh:current >/dev/null 2>&1
+0 8 * * * php app/console rant:stats:nfl:refresh:current >/dev/null 2>&1
+0 8 * * * php app/console rant:stats:nhl:refresh:current >/dev/null 2>&1
 
 cron "cbb_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cbb:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cbb:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 cron "cfb_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cfb:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cfb:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 cron "mlb_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:mlb:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:mlb:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 cron "nba_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nba:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nba:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 cron "nfl_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nfl:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nfl:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 cron "nhl_refresh_games" do
-  minute "*/5"
-  hour "0-3,8-23"
+  minute "0"
+  hour "8"
   action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nhl:refresh:games:current >> #{log_file} 2>&1"
+  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nhl:refresh:current >> #{log_file} 2>&1"
   user "root"
 end
 
-# Refresh current teams every 15 minutes during active hours
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:cbb:refresh:teams:current >/dev/null 2>&1
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:cfb:refresh:teams:current >/dev/null 2>&1
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:mlb:refresh:teams:current >/dev/null 2>&1
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:nba:refresh:teams:current >/dev/null 2>&1
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:nfl:refresh:teams:current >/dev/null 2>&1
-# 3,18,33,48 0-3,8-23 * * * php app/console rant:stats:nhl:refresh:teams:current >/dev/null 2>&1
-
-cron "cbb_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cbb:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-cron "cfb_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cfb:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-cron "mlb_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:mlb:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-cron "nba_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nba:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-cron "nfl_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nfl:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-cron "nhl_refresh_teams" do
-  minute "3,18,33,48"
-  hour "0-3,8-23"
-  action :create
-  command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nhl:refresh:teams:current >> #{log_file} 2>&1"
-  user "root"
-end
-
-# Refresh all data once a night at 3:00 am
-# 0 3 * * * php app/console rant:stats:cbb:refresh:all >/dev/null 2>&1
-# 0 3 * * * php app/console rant:stats:cfb:refresh:all >/dev/null 2>&1
-# 0 3 * * * php app/console rant:stats:mlb:refresh:all >/dev/null 2>&1
-# 0 3 * * * php app/console rant:stats:nba:refresh:all >/dev/null 2>&1
-# 0 3 * * * php app/console rant:stats:nfl:refresh:all >/dev/null 2>&1
-# 0 3 * * * php app/console rant:stats:nhl:refresh:all >/dev/null 2>&1
+# Refresh all data once a night at 3:15 am
+# 15 3 * * * php app/console rant:stats:cbb:refresh:all >/dev/null 2>&1
+# 15 3 * * * php app/console rant:stats:cfb:refresh:all >/dev/null 2>&1
+# 15 3 * * * php app/console rant:stats:mlb:refresh:all >/dev/null 2>&1
+# 15 3 * * * php app/console rant:stats:nba:refresh:all >/dev/null 2>&1
+# 15 3 * * * php app/console rant:stats:nfl:refresh:all >/dev/null 2>&1
+# 15 3 * * * php app/console rant:stats:nhl:refresh:all >/dev/null 2>&1
 
 cron "cbb_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cbb:refresh:all >> #{log_file} 2>&1"
   user "root"
 end
 cron "cfb_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:cfb:refresh:all >> #{log_file} 2>&1"
   user "root"
 end
 cron "mlb_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:mlb:refresh:all >> #{log_file} 2>&1"
   user "root"
 end
 cron "nba_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nba:refresh:all >> #{log_file} 2>&1"
   user "root"
 end
 cron "nfl_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nfl:refresh:all >> #{log_file} 2>&1"
   user "root"
 end
 cron "nhl_refresh_all" do
-  minute "0"
+  minute "15"
   hour "3"
   action :create
   command "#{app_web_root}/current/app/console --env=#{app_env} rant:stats:nhl:refresh:all >> #{log_file} 2>&1"
